7b9cdf8bfeef96c0dd402690855f2fa2683c69b8
Ritik Katkam <rithikkatkam123@gmail.com>
2021-12-26 23:23:01 +0530

    initial commit

--- myprogram.java
+++ myprogram.java
@@ -2,7 +2,7 @@ public class myprogram{
     public static void main(String args[]){
         int a=5;
         int b=6;
-        System.out.println(a+b);
+        System.out.println("this sum is"+(a+b));
         
     }
 }

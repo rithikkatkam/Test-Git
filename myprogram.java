e98cb1a89d38e2f033258e8cd826fca2791e2d95
Ritik Katkam <rithikkatkam123@gmail.com>
2021-12-26 23:31:27 +0530

    Removed the text

--- myprogram.java
+++ myprogram.java
@@ -2,7 +2,7 @@ public class myprogram{
     public static void main(String args[]){
         int a=5;
         int b=6;
-        System.out.println("this sum is"+(a+b));
+        System.out.println((a+b));
         
     }
 }
